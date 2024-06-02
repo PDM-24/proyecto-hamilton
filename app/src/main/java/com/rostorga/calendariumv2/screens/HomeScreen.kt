81fde0a300a66ca81ef581dd7c4adb6df574e693
RaulOstorga <00380821@uca.edu.sv>
2024-06-01 20:59:01 -0600

    :sparkles:

--- app/src/main/java/com/rostorga/calendariumv2/screens/HomeScreen.kt
+++ app/src/main/java/com/rostorga/calendariumv2/screens/HomeScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -71,40 +72,118 @@ fun ToolBar() {
 
 @Composable
 fun FAB() {
+    var showDialog by remember { mutableStateOf(false) }
+    var showCalendar by remember { mutableStateOf(false) }
 
-    val context = LocalContext.current
+
+    if (showDialog) {
+        AddTaskPopUp(
+            onDismiss = { showDialog = false },
+            onNext = {
+                showDialog = false
+                showCalendar = true
+            }
+        )
+    }
+    if (showCalendar) {
+        CalendarDialogPopUp(onDismiss = { showCalendar = false })
+    }
+
     FloatingActionButton(
-        onClick = {
-            Toast.makeText(context, "Button clicked", Toast.LENGTH_SHORT).show()
-        },
+        onClick = { showDialog = true },
         shape = CircleShape
     ) {
         Icon(imageVector = Icons.Filled.Add, contentDescription = "Add")
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddTaskPopUp(
+    onDismiss: () -> Unit,
+    onNext: () -> Unit
+
+) {
+    var task by remember { mutableStateOf("") }
+    var time by remember { mutableStateOf("12:00") }
+    val timeState1  = rememberTimePickerState(9, 15, false)
+    val timeState2  = rememberTimePickerState(9, 15, false)
+
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            //this button should open up another display that shows a calendar
+            Button(onClick = onNext) {
+                Text("NEXT")
+            }
+        },
+        modifier = Modifier.height(500.dp),
+        title = {
+            Text("Add New Task")
+        },
+        text = {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                TimeInput(state = timeState1)
+                TimeInput(state = timeState2)
+
+                Spacer(modifier=Modifier.padding(16.dp))
+                OutlinedTextField(
+                    value = task,
+                    onValueChange = { task = it },
+                    shape= RoundedCornerShape(12.dp),
+                    placeholder = { Text(text = "Add a description :) ") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                        .height(100.dp)
+                )
+
+            }
+        }
+    )
+}
+
+@Composable
+fun CalendarDialogPopUp(
+    onDismiss:()-> Unit
+){
+    var date by remember { mutableStateOf(" ")}
+    AlertDialog(onDismissRequest = onDismiss , confirmButton = {
+        //center the button but yeah mostly works now :D
+            Button(onClick=onDismiss ){
+                Text(text="OK")
+    }
+
+    },
+        text={
+            AndroidView(factory = { CalendarView(it)}, update = { it.setOnDateChangeListener{ _, year, month, day -> date = "$day - ${month + 1} - $year" }}
+            )
+        }
+    )
+
+
+}
+
+
 @RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreenContent() {
-    var date by remember {
-        mutableStateOf(" ")
-    }
+    var date by remember { mutableStateOf(" ") }
 
     val stroke = Stroke(
         width = 2f,
         pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
     )
 
-
-
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
-
         Text("TODO CREATE THE LITTLE BAR THAT TRACKS THE WORKLOAD OF THE FIGMA")
         Text("TODO JOIN OR CREATE A TEAM LIKE IN THE FIGMA")
 
@@ -141,7 +220,7 @@ fun HomeScreenContent() {
                 },
             contentAlignment = Alignment.Center
         ) {
-            Text(textAlign = TextAlign.Center, text = "Tap here to introduce yourself")
+            Text(textAlign = TextAlign.Center, text = "Add a new task")
         }
 
         Spacer(modifier = Modifier.padding(8.dp))
---
+++ app/src/main/java/com/rostorga/calendariumv2/screens/ProfileScreen.kt
@@ -0,0 +1,56 @@
+package com.rostorga.calendariumv2.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+//IMPORTANT
+/*
+    onDismiss:()-> Unit
+
+            AlertDialog(onDismissRequest = onDismiss, confirmButton = {
+                Button(onClick = onDismiss)
+                {
+                    Text(text = "OK")
+                }
+
+
+            }
+            )*/
+
+
+@Preview
+@Composable
+fun profileScreen( ){
+
+    Column(modifier=Modifier.padding(15.dp).fillMaxSize() ){
+        Box(modifier=Modifier.height(200.dp).width(300.dp).background(color= Color.Red)
+        ){
+
+
+        }
+
+    }
+
+
+
+
+}
