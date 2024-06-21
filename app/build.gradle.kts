4b0962892b0adcbca51b1af83212effe64f1ec42
RaulOstorga <00380821@uca.edu.sv>
2024-06-21 08:36:18 -0600

    only 4 days left! sadge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,21 +56,16 @@ dependencies {
     val room_version = "2.6.1"
     val nav_version = "2.7.7"
     val lifecycle_version = "2.8.1"
-    val retrofit_version = "2.11.0"
 
 
     implementation("androidx.room:room-runtime:$room_version")
-
     // To use Kotlin Symbol Processing (KSP)
     ksp("androidx.room:room-compiler:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
 
-    //RetroFit
-    implementation ("com.squareup.retrofit2:retrofit:$retrofit_version")
-    implementation ("com.squareup.retrofit2:converter-gson:$retrofit_version")
+    implementation("androidx.navigation:navigation-common:2.7.7")
 
-    //okhttp3
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
 
 
     implementation("androidx.navigation:navigation-compose:$nav_version")
