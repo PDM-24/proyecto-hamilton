396990725b70af46e7c629975c35376003c2acbf
OscarOstorga <00037422@uca.edu.sv>
2024-06-21 08:36:48 -0600

    m

--- app/src/main/java/com/rostorga/calendariumv2/api/ApiClient.kt
+++ app/src/main/java/com/rostorga/calendariumv2/api/ApiClient.kt
@@ -1,6 +1,5 @@
 package com.rostorga.calendariumv2.api
 
-import com.rostorga.taller2moviles.util.Constants
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
--- app/src/main/java/com/rostorga/calendariumv2/api/apiObject/UserApiObject.kt
+++ app/src/main/java/com/rostorga/calendariumv2/api/apiObject/UserApiObject.kt
@@ -3,13 +3,13 @@ package com.rostorga.calendariumv2.api.apiObject
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-data class userApiObject(
+data class UserApiObject(
     @SerializedName(value = "_id")
     @Expose
     val id: String? = null,
     @SerializedName(value = "Name")
     @Expose
-    val name: userNameApiObject,
+    val name: UserNameApiObject,
     @SerializedName(value = "UserName")
     val userName: String,
     @SerializedName(value = "Password")
@@ -17,11 +17,11 @@ data class userApiObject(
     val password: String
 )
 
-userNameApiObject(
-    @SerializedName(value = 'first')
+data class UserNameApiObject(
+    @SerializedName(value = "first")
     @Expose
     val frist: String,
-    @SerializedName(value = 'last')
+    @SerializedName(value = "last")
     @Expose
     val last: String,
 )
